96a821393d942d9634be74b5eb7fa35c8b9b6c7b
Arundhati Bandopadhyaya <38293989+Arundhati1234@users.noreply.github.com>
2020-12-02 12:45:25 +0530

    final code

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN
 speaker = pyttsx3.init()
 for num in range(7, pages):
     page = pdfReader.getPage(num)
-    text = page.extractText()
-    voices = speaker.getProperty('voices')
-    for voice in voices:
-        print(voice)
-    
+    text = page.extractText()    
     speaker.say(text)
     speaker.setProperty('voice', voice_id)
     speaker.setProperty('rate', 150)
